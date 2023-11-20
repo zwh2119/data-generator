db424bf0d3d9f671ea47c942882d949a5aec6015
onecheck <1548767361@qq.com>
2023-11-20 22:24:30 +0800

    update AutoEdge v0.0.2

--- video_generator.py
+++ video_generator.py
@@ -56,9 +56,30 @@ class VideoGenerator(Generator):
                 # compress all the frames in the buffer into a short video, send it as a task, and empty the buffer
                 compressed_video = self.compress_frames(temp_frame_buffer, frame_fourcc)
                 base64_frame = base64.b64encode(compressed_video).decode('utf-8')
-                task = VideoTask(base64_frame, cur_id, self.generator_id, self.priority, tuned_parameters, self.task_dag)
+                task = VideoTask(base64_frame, cur_id, self.generator_id, self.priority, tuned_parameters,
+                                 self.task_dag)
 
                 # TODO: post task to local controller
+                """
+                data structure
+                
+                1.source_id
+                2.task_id
+                3.priority 
+                4.metadata:{resolution, frame rate,
+                            frame number, encoding}
+                
+                5.pipeline_flow:[service1, service2,..., end]
+                    service:{service_name, execute_address, execute_data}
+                    execute_data:{service_time, transmit_time, acc}
+                6.cur_flow_index
+                7.scenario_data:{obj_num, obj_size, stable}
+                8.content_data (frame/middle_result/result)
+                9.tmp_data:{} (middle_record)
+                
+                
+                
+                """
 
                 cur_id += 1
                 temp_frame_buffer = []
@@ -77,3 +98,14 @@ class VideoGenerator(Generator):
         # delete the temporary file
         os.remove(buffer_path)
         return compressed_video
+
+    def generate_blank_data(self):
+        data_structure = {}
+        data_structure['source_id'] = -1
+        data_structure['task_id'] = -1
+        data_structure['priority'] = -1
+        data_structure['metadata'] = {}
+        data_structure['pipeline_flow'] = []
+        data_structure['cur_flow_index'] = 0
+
+        return data_structure
