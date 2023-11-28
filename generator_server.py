a6f7e4948fc4531934d8253fac39c149ea990dde
onecheck <1548767361@qq.com>
2023-11-29 00:55:54 +0800

    update AutoEdge v0.1.4

--- generator_server.py
+++ generator_server.py
@@ -2,11 +2,12 @@ import yaml_utils
 import threading
 from video_generator import VideoGenerator
 
+scheduler_address = 'http://114.212.81.11:8140/schedule'
+
 
 def main():
     video_config = yaml_utils.read_yaml('video_config.yaml')
     videos = video_config['video']
-    scheduler_address = 'http://114.212.81.11:8140/schedule'
 
     for video in videos:
         video_generator = VideoGenerator(video['url'], video['id'], 0, video['pipeline'],
